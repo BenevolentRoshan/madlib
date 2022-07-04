2940f4a126f49d766fd3b2a9588eddf2c69f5536
BenevolentRoshan <95609741+BenevolentRoshan@users.noreply.github.com>
2022-07-04 16:58:25 +0530

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,7 +5,6 @@ verb1=input("Enter the verb: ")
 food=input("Enter the food: ")
 name=input("Enter the name: ")
 
-
 print(f"food was --{adj}--. "
       f"She --{verb1}--, ___{verb2}___, and ___{verb}___ through the playground."
       f" Luckily, ___{name}___’s friends were willing to share their ___{food}___ with her.")
