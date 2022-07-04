c5fb3a59c275374aeb72de288fa56d0b683aa7ed
BenevolentRoshan <95609741+BenevolentRoshan@users.noreply.github.com>
2022-07-04 16:56:26 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+verb=input("Enter the verb: ")
+adj=input("Enter the adjective: ")
+verb2=input("Enter the verb: ")
+verb1=input("Enter the verb: ")
+food=input("Enter the food: ")
+name=input("Enter the name: ")
+
+
+print(f"food was --{adj}--. "
+      f"She --{verb1}--, ___{verb2}___, and ___{verb}___ through the playground."
+      f" Luckily, ___{name}___’s friends were willing to share their ___{food}___ with her.")
